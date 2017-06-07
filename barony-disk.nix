4d9acce5299c180abff094e305155565729f6c6d
Linus Heckemann <git@sphalerite.org>
2017-06-07 20:05:44 +0100

    Working iso with read-write directories

--- barony-disk.nix
+++ barony-disk.nix
@@ -1,19 +1,29 @@
-let 
+let
 pkgs = import <nixpkgs> {};
 isoWritableModule = { config, pkgs, lib, ... }:
 {
   config = {
-    fileSystems."/nix/.rw-store" = lib.mkForce {
+    fileSystems."/0-rw" = {
       fsType = "ext4";
       neededForBoot = true;
       device = "/dev/disk/by-label/NIXOS_RW";
+    };
+    fileSystems."/nix/.rw-store" = lib.mkForce {
+      options = [ "bind" ];
+      neededForBoot = true;
+      device = "/0-rw/nix-store";
+    };
+    fileSystems."/home" = {
+      options = [ "bind" ];
+      neededForBoot = true;
+      device = "/0-rw/home";
     };
   };
 };
 config = (import <nixpkgs/nixos/lib/eval-config.nix> {
   inherit (pkgs) system;
-  modules = [ 
-    ./barony-config.nix 
+  modules = [
+    ./barony-config.nix
     isoWritableModule
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
   ];
@@ -22,14 +32,22 @@ isoImage = config.system.build.isoImage;
 in
   with pkgs; stdenv.mkDerivation {
     name = "isoWritable.iso";
-    src = /var/empty;
     buildInputs = [ e2fsprogs utillinux ];
-    buildPhase = ''
-      dd if=/dev/zero of=store-rw bs=1M count=1024
-      mkfs.ext4 store-rw -L NIXOS_RW
-      cat ${config.system.build.isoImage}/iso/*.iso store-rw > $out
-      sfdisk $out <<EOF
-        type=L
+    buildCommand = ''
+      cp ${config.system.build.isoImage}/iso/*.iso $out
+      chmod u+w $out
+      dd if=/dev/zero of=$out bs=1M count=1024 oflag=append conv=notrunc
+      sfdisk --append $out <<EOF
+        part3 : type=83
       EOF
+      offset_sectors=$(sfdisk -l -q -o start $out | tail -n 1)
+      offset=$((512 * $offset_sectors))
+      dd if=$out of=rw-fs bs=$offset skip=1
+      mkfs.ext4 -L NIXOS_RW rw-fs
+      debugfs -w rw-fs -f /dev/stdin <<EOF
+        mkdir nix-store
+        mkdir home
+      EOF
+      dd if=rw-fs of=$out conv=notrunc bs=$offset seek=1
     '';
   }
